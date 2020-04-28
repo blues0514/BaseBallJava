40caed3d208a9e466234be3f092fb3545fd41202
blues0514 <blues0514@naver.com>
2020-04-28 11:03:17 +0900

    Add Prac_Baseball [LJH ADD]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Random;
 import java.util.Scanner;
 
-// 야구게임
+// 야구 게임
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
@@ -11,6 +11,7 @@ public class Main {
         int nums[] = new int[3];
         boolean run = true;
 
+        // 정답 생성
         while (run) {
             for (int i = 0; i < rNums.length; i++) {
                 int rNum = r.nextInt(9) + 1;
@@ -22,11 +23,18 @@ public class Main {
             }
         }
 
+        // 추측 입력
         Scanner sc = new Scanner(System.in);
         System.out.println("숫자 3개 입력");
         for (int i = 0; i < nums.length; i++) {
             nums[i] = sc.nextInt();
         }
+
+        // 결과 계산
+
+
+
+        // 도전 횟수 출력
 
     }
 }
