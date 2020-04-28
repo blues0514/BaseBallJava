8102cd5a8283de9effb459993cd634311bb5e649
blues0514 <blues0514@naver.com>
2020-04-28 16:15:50 +0900

    Add 7j-method [LJH ADD]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,57 +1,29 @@
 package com.company;
 
 public class Main {
+    public static final int DIGIT = 3;
+    public static final int MAX_NUMBER = 10;
 
     public static void main(String[] args) {
-        int[] answers = Module.generateAnswers();
-        Module.printNumbers("[정답]", answers);
+        Answer answer = new Answer();
+        answer.generate();
+        answer.print();
 
         int tryCount = 0;
         while (true) {
             tryCount++;
 
-            int[] guesses = Module.inputGuesses();
-            Module.printNumbers("[추측]", guesses);
+            Guess guess = new Guess();
+            guess.input();
+            guess.print();
 
             Result result = new Result();
-            calculateResult(result, answers, guesses);
+            result.calculate(answer, guess);
+            result.print();
 
-            printResult(result);
-
-            if (isGoodResult(result))
+            if (result.isGood())
                 break;
         }
         System.out.println("도전 횟수 : " + tryCount);
     }
-
-    /**
-     * 결과를 계산한다.
-     *
-     * @param result  결과 값
-     * @param answers 정답 값
-     * @param guesses 추측 값
-     */
-    private static void calculateResult(Result result, int[] answers, int[] guesses) {
-        for (int i = 0; i < Module.DIGIT; i++) {
-            int j = (i + 1) % Module.DIGIT; // (2+1) % 3 = 0
-            int k = (i + 2) % Module.DIGIT; // (2+2) % 3 = 1
-
-            if (guesses[i] == answers[i])
-                result.strike++;
-            else if (guesses[i] == answers[j] || guesses[i] == answers[k])
-                result.ball++;
-            else
-                result.out++;
-        }
-    }
-
-    private static boolean isGoodResult(Result result) {
-        return result.strike == Module.DIGIT;
-    }
-
-    private static void printResult(Result result) {
-        System.out.println("S:" + result.strike + " B:" + result.ball + " O:" + result.out);
-    }
-
-
 }
